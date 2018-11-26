1b1d428c69166c1486efabc83e36909815e7f9f9
Carine <cilungaw@wellesley.edu>
2018-11-26 22:29:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log('HELLO')}
 function logWhisper(string) {console.log('hello')}
-a
+function helloGrandma(string){
+  if(string == 'hello'){
+    return "I can't hear you"
+  }
+}
